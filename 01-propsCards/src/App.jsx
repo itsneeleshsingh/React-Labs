433ff3529e19e6950f3d96518211d955b1b1834e
itsneeleshsingh <neeleshsinghdpsjkp.2483ns@gmail.com>
2026-08-30 18:30:56 +0530

    React props init

---
+++ 01-propsCards/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={()=>setCount(count+1)}>Click</button>
+    </div>
+  )
+}
+
+export default App
